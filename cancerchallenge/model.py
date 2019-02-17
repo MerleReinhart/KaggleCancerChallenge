853575c4e8dc3841c489ef18c7e24ab9a7a6b5a3
Merle Reinhart <merle.reinhart@icr.ac.uk>
2019-02-17 18:08:25 -0500

    defined model, first try

--- cancerchallenge/model.py
+++ cancerchallenge/model.py
@@ -1,5 +1,21 @@
-import torch
+import torch.nn as nn
 
 
-class Model(torch.nn.Module):
-    pass
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.layer1 = nn.Sequential(nn.Conv2d(3, 20, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(nn.Conv2d(20, 20, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(nn.Conv2d(20, 10, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2))
+        self.layer4 = nn.Sequential(nn.Linear(10 * 12 * 12, 200), nn.ReLU())
+        self.layer5 = nn.Sequential(nn.Linear(200, 1), nn.Sigmoid())
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+
+        return out
